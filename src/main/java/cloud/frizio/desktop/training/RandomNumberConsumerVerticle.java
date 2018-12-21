42478ef84c0d37df23b54e0d9bf1278d56131634
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-21 14:12:02 +0100

    Create consumer process

---
+++ src/main/java/cloud/frizio/desktop/training/RandomNumberConsumerVerticle.java
@@ -0,0 +1,28 @@
+package cloud.frizio.desktop.training;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+
+/**
+ * RandomNumberConsumerVericle
+ */
+public class RandomNumberConsumerVerticle extends AbstractVerticle {
+    
+    public void start() throws Exception {
+        
+        EventBus eventBus = getVertx().eventBus();
+		
+        eventBus.<Integer>consumer(
+            "javaboss.channel", 
+            message -> {
+                System.out.println("CONSUMER: I have received the message: " + message.body());
+                if ( message.body() >= 50 ) {
+                    message.fail( 0, "I don't like it!");
+                } else {
+                    message.reply( "ok!" );
+                }
+            }
+        );
+    }
+    
+}
